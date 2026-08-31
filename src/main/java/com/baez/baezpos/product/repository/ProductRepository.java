e04f8b27451e738b5058470bd0828a798e63054f
Alexander Baez <alexanderbaez146@gmail.com>
2026-08-30 21:03:37 -0300

    Liberamos la memoria ram

--- src/main/java/com/baez/baezpos/product/repository/ProductRepository.java
+++ src/main/java/com/baez/baezpos/product/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.baez.baezpos.product.repository;
 
 import com.baez.baezpos.product.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
